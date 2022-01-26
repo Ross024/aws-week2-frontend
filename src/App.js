2f6cff3d953f6405a9e37db2876a627745b6bc28
Ross Scharbach <rscharbach1@my.bcit.ca>
2022-01-25 21:12:19 -0800

    frontend image posting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import './App.css'
 
-function App() {
+export default function App() {
+  const [file, setFile] = useState()
+  const [description, setDescription] = useState("")
+  const [image, setImage] = useState()
+
+  const submit = async event => {
+    event.preventDefault()
+  
+    const formData = new FormData()
+    formData.append("image", file)
+    formData.append("description", description)
+  
+    const result = await axios.post('/api/images', formData, { headers: {'Content-Type': 'multipart/form-data'}})
+    setImage(result.data.imagePath)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submit}>
+        <input
+          filename={file} 
+          onChange={e => setFile(e.target.files[0])} 
+          type="file" 
+          accept="image/*"
+        ></input>
+        <input
+          onChange={e => setDescription(e.target.value)} 
+          type="text"
+        ></input>
+        <button type="submit">Submit</button>
+      </form>
+      { image && <img src={image} />}
     </div>
-  );
+  )
 }
 
-export default App;
